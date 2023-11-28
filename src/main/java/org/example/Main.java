11f4f91893b5a058da2750970668b90bcc72316a
dicojo <diniercj@gmail.com>
2023-11-28 11:30:15 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+   //Natillera la abuelita toretto
+   //¿Que nesesito para procesar la natillera del cliente?
+   //ENTRADAS
+    int identificadorCliente=11233233;
+    long documentoIdentidad=1109291287987L;
+    byte edadCliente=19;
+    short cuotaMensualCliente=25000;
+    float tasaInteresFijo=0.1f;
+    char calificacionCliente='A';
+
+
+
+    }
+}
